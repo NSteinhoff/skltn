70b037496140dd2e9e6d71508835390f0c85bc02
ksonj <kson@siberie.de>
2016-02-01 14:52:59 +0100

    Change year to 2016, try to guess author details from git config

--- skltn/metadata.py
+++ skltn/metadata.py
@@ -3,6 +3,17 @@
 
 Information describing the project.
 """
+import subprocess
+
+
+def get_author_detail(arg='name'):
+    p = subprocess.Popen(['git', 'config', 'user.{}'.format(arg)],
+                         stdout=subprocess.PIPE)
+    try:
+        out, _ = p.communicate()
+    except:
+        out = ''
+    return out.strip() or None
 
 # The package name, which is also the "UNIX name" for the project.
 package = 'my_module'
@@ -10,9 +21,9 @@ project = "My Awesome Module"
 project_no_spaces = project.replace(' ', '')
 version = '0.1.0'
 description = 'It does cool things'
-authors = ['John Doe']
+authors = [get_author_detail('name') or 'John Doe']
 authors_string = ', '.join(authors)
-emails = ['foobar@example.com', 'johndoe@thisisfake.org']
+emails = [get_author_detail('email') or 'doe@example.org']
 license = 'MIT'
-copyright = '2015 ' + authors_string
+copyright = '2016 ' + authors_string
 url = 'http://example.com/'
